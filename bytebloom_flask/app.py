498c5562036a9cd1e68bcdc1a1290981dfef9b26
twdearth-wtcc <twdearth@my.waketech.edu>
2024-02-09 17:53:41 -0500

    Adds simple login mechanics
    Login information should be stored in bytebloom_flask/.env. System currently supports one admin and one cashier only. Will be replaced and support multiple users when a database is implemented in sprint 2.

--- bytebloom_flask/app.py
+++ bytebloom_flask/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request
+from dotenv import dotenv_values
 
 # Startup functions
-
 app = Flask(__name__)
+secrets = dotenv_values()
 
 @app.get("/")
 def get_login_page():
@@ -11,4 +12,11 @@ def get_login_page():
 @app.post("/")
 def login_user():
     print(request.form)
-    return render_template('login_page.html')
+    username = request.form['username']
+    password = request.form['password']
+    if username == secrets['ADMIN_USERNAME'] and password == secrets['ADMIN_PASSWORD']:
+        return f"<h1>Welcome, {username}.<h1><p>Logged in as ADMIN</p>"
+    elif username == secrets['CASHIER_USERNAME'] and password == secrets['CASHIER_PASSWORD']:
+        return f"<h1>Welcome, {username}.<h1><p>Logged in as CASHIER</p>"
+    else:
+        return "<h1>Your username or password was incorrect.</h1>"
