03a9be95c71b2c615e00dd267e40793bc8de2451
twdearth-wtcc <twdearth@my.waketech.edu>
2024-02-18 15:10:18 -0500

    Refactors routing to reduce redundancy
    Co-authored by: nyjames

--- bytebloom_flask/app.py
+++ bytebloom_flask/app.py
@@ -53,52 +53,16 @@ def initialize_database():
     with app.app_context():
         db.create_all()
 
-# call the initialize_database function and pull up login
-        
-initialize_database()
-
 @app.route("/", methods=["GET"])
 
 def get_login_page():
     return render_template('login_page.html')
 
-# use forms to get username and pasword
-
-@app.route("/", methods=["POST"])
-def login_user():
-    username = request.form.get('username')
-    password = request.form.het('password')
-
-# query to find matching user/pw in database
-    
-    user = UserCredential.query.filter_by(username=username, Password=password).first()
-
-    if user:
-        return render_template('Manager_UI.html' if user.employee.FirstName == "Zineb" else 'Cashier_UI.html', username=username)
-
-# App Functions --------------------  
-
-# @app.get("/")
-# def get_login_page():
-    # return render_template('login_page.html')
-
-# @app.post("/")
-#def login_user():
-##    username = request.form['username']
-#    password = request.form['password']
-#    if username == secrets['ADMIN_USERNAME'] and password == secrets['ADMIN_PASSWORD']:
-#        return render_template('Manager_UI.html', username = username)
-#    elif username == secrets['CASHIER_USERNAME'] and password == secrets['CASHIER_PASSWORD']:
-#        return render_template('Cashier_UI.html', username = username)
-#    else:
-#        return "<h1>Your username or password was incorrect.</h1>"
-
-#----------------------------------
-
 # Route to handle form submission for login
 @app.route("/", methods=["POST"])
 
 def login_user():
+    # Use forms to get username and pasword
     username = request.form.get('username')
     password = request.form.get('password')
     
@@ -109,3 +73,7 @@ def login_user():
         return render_template('Manager_UI.html' if user.employee.FirstName == "Manager" else 'Cashier_UI.html', username=username)
     else:
         return "<h1>Your username or password was incorrect.</h1>"
+
+
+# call the initialize_database function and pull up login
+initialize_database()
