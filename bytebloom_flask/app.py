49dc47d921eaecc6effb2247cdfe8234cbea01dd
twdearth-wtcc <twdearth@my.waketech.edu>
2024-02-20 12:43:28 -0500

    Refactors SQLAlchemy model into independent module
    This commit moves SQLAlchemy classes, methods, and CLI tools to db_models.py and provides docstrings for existing ORM classes.

--- bytebloom_flask/app.py
+++ bytebloom_flask/app.py
@@ -1,57 +1,12 @@
 from flask import Flask, render_template, request
 
-# database ------------------------------
-
-from flask_sqlalchemy import SQLAlchemy
-
 # Startup functions / --------------------
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bytebloom.db'
 
-# Database Information/classes --------
-
-db = SQLAlchemy(app)
-
-class Employee(db.Model):
-
-    __tablename__ = 'Employees'
-
-    EmployeeID = db.Column(db.Integer, primary_key=True)
-    FirstName = db.Column(db.String(50))
-    LastName = db.Column(db.String(50))
-    Email = db.Column(db.String(100))
-    credential = db.relationship("UserCredential", back_populates="employee")
-
-# ------------------------------------------------------------
-# Model temporarily disabled until employee login is debugged
-# ------------------------------------------------------------
-# class MenuItem(db.Model):
-#     __tablename__ = 'MenuItems'
-
-#     MenuItemID = db.Column(db.Integer, primary_key=True)
-#     Name = db.Column(db.String(255), nullable=False)
-#     ExpirationDate = db.Column(db.Date)
-#     Pricing = db.Column(db.Numeric(10, 2))
-#     Quantity = db.Column(db.Integer)
-#     EmployeeID = db.Column(db.Integer, db.ForeignKey('Employees.EmployeeID'))  
-
-#     employee = db.relationship("Employee")
-
-class UserCredential(db.Model):
-
-    __tablename__ = 'UserCredentials'
-    CredentialID = db.Column(db.Integer, primary_key=True)
-    Username = db.Column(db.String(50), unique=True, nullable=False)
-    Password = db.Column(db.String(50), nullable=False)
-    EmployeeID = db.Column(db.Integer, db.ForeignKey('Employees.EmployeeID'))
-    employee = db.relationship("Employee", back_populates="credential")
+from db_models import *     # Imports objects from the database *after* they have been added to the database
 
 # documentation info ---------------
-    # create database tables if they do not exist
-def initialize_database():  
-    with app.app_context():
-        db.create_all()
 
 @app.route("/", methods=["GET"])
 
@@ -74,27 +29,3 @@ def login_user():
         return render_template('Manager_UI.html' if valid_credential.employee.FirstName == "Manager" else 'Cashier_UI.html', username=username)
     else:
         return "<h1>Your username or password was incorrect.</h1>"
-
-
-# CLI tool to drop all tables for testing. Run with "flask db_drop"
-# TODO: Delete this
-@app.cli.command('db_drop')
-def drop_all():
-    db.drop_all()
-
-# CLI tool to populate table with test data. Run with "flask db_populate"
-@app.cli.command("db_populate")
-def populate_users():
-    test_manager = Employee(EmployeeID=1, FirstName="Manager", LastName="Gamra", Email="test@test.org")
-    test_manager_credentials = UserCredential(Username="Manager", Password="foo", EmployeeID=1)
-    test_cashier = Employee(EmployeeID=2, FirstName="Nya", LastName="James", Email="test@test.org")
-    test_cashier_credentials = UserCredential(Username="Cashier", Password="bar", EmployeeID=2)
-
-    db.session.add(test_manager)
-    db.session.add(test_manager_credentials)
-    db.session.add(test_cashier)
-    db.session.add(test_cashier_credentials)
-    db.session.commit()
-
-# call the initialize_database function and pull up login
-initialize_database()
